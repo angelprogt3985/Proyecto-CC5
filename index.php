31a76ac583a0244fee564feef0a0613b902354b9
Angel Camargo <angel.camargo@galileo.edu>
2026-05-07 20:54:41 -0600

    Inicio proyecto

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiniela</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="menu">
+
+        <h1>Quiniela</h1>
+
+        <button>Ver Quiniela</button>
+        <button>Agregar Equipo</button>
+        <button>Editar Equipo</button>
+        <button>Ver Resultados</button>
+
+    </div>
+
+</body>
+</html>
